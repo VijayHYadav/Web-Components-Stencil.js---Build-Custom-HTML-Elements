4e51e9061a1d57014509b80bafe193e133321e01
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-26 08:22:15 +0530

    basics-07-using-connectedcallback-for-dom-access

--- tooltip.js
+++ tooltip.js
@@ -1,10 +1,14 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
+    }
+
+    connectedCallback() {
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?)';
         // to access web component object
         this.appendChild(tooltipIcon);
+
     }
 }
 
