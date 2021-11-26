05ca8d045dbaf3940dae66a38063812d75d46acf
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-26 08:32:38 +0530

    basics-08-listening-to-events-inside-the-component.

--- tooltip.js
+++ tooltip.js
@@ -1,14 +1,28 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
+        this._tooltipContainer;
     }
 
     connectedCallback() {
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         // to access web component object
         this.appendChild(tooltipIcon);
 
+    }
+
+    // this indicates means this method will only call by this class itself.
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = 'This is tooltip text!';
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.removeChild(this._tooltipContainer);
     }
 }
 
