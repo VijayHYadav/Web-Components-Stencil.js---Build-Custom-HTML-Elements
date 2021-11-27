5addfbf242001641ff12a4dfa061ce9a054ada93
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-27 13:20:44 +0530

    advanced-14-wrap-up.

--- tooltip.js
+++ tooltip.js
@@ -27,6 +27,10 @@ class Tooltip extends HTMLElement {
                     box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
                 }
 
+                :host {
+                    position: relative;
+                }
+
                 :host(.important) {
                     background: var(--color-primary, #ccc);
                     padding: 0.15rem;
@@ -62,9 +66,7 @@ class Tooltip extends HTMLElement {
         this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         // to access web component object
         this.shadowRoot.appendChild(this._tooltipIcon);
-        this.style.position = 'relative'
         this._render();
-
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
