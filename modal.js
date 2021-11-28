da18155aae86eb750b4244ebd54b2fa10bec2885
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-28 09:36:14 +0530

    complex-cmp-09-slotchange-and-getting-access-to-slot-content.

--- modal.js
+++ modal.js
@@ -76,6 +76,12 @@ class Modal extends HTMLElement {
                 </section>
             </div>
         `;
+
+        const slots = this.shadowRoot.querySelectorAll('slot');
+        slots[1].addEventListener('slotchange', event => {
+            console.dir(slots[1]);
+            console.dir(slots[1].assignedNodes());
+        })
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
