b9dce86029a40bc1d0b54588602caf8ec0d94b05
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-28 11:09:07 +0530

    complex-cmp-11-dispatching-custom-events.

--- modal.js
+++ modal.js
@@ -87,6 +87,9 @@ class Modal extends HTMLElement {
         const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
         cancelButton.addEventListener('click', this._cancel.bind(this));
         confirmButton.addEventListener('click', this._confirm.bind(this));
+        cancelButton.addEventListener('cancel', () => {
+            console.log('Cancel inside')
+        })
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -120,8 +123,10 @@ class Modal extends HTMLElement {
         this.isOpen = false;
     }
 
-    _cancel() {
+    _cancel(event) {
         this.hide();
+        const cancelEvent = new Event('cancel');
+        event.target.dispatchEvent(cancelEvent);
     }
 
     _confirm() {
